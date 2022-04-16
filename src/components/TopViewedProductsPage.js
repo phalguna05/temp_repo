7522e56cf5b5c83739dbb01fc66f58e5e316d254
phalguna <phalgunakadukuntla05@gmail.com>
2022-04-16 09:26:45 +0530

    changes to the chart

--- src/components/TopViewedProductsPage.js
+++ src/components/TopViewedProductsPage.js
@@ -1,5 +1,9 @@
 import _ from 'lodash';
 import React from 'react';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import { Pie } from 'react-chartjs-2';
 import ProductApi from '../data/ProductApi';
 import Header from '../includes/header';
 
@@ -7,76 +11,64 @@ export default class TopViewedProductsPage extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			sortedList: [],
+			data: {},
 		};
 	}
 
 	componentDidMount() {
 		ProductApi.getProductViews((productViews) => {
 			const grouped = _(productViews).groupBy('productName').value();
-			let arr = [];
-			for (let [key, value] of Object.entries(grouped)) {
-				let tempObj = {
-					name: key,
-					count: value.length,
-				};
-				arr.push(tempObj);
-			}
-			arr.sort(compare);
-			this.setState({ sortedList: arr });
+			let labelArray = Object.keys(grouped);
+			const colors = getBackgroundColors(Object.keys(labelArray).length);
+			const data = {
+				labels: labelArray,
+				datasets: [
+					{
+						label: 'Product Views',
+						backgroundColor: colors,
+						borderColor: colors,
+						borderWidth: 1,
+
+						data: Object.keys(grouped).map((key) => grouped[key].length),
+					},
+				],
+			};
+			this.setState({ data });
 		});
 	}
 
 	render() {
 		return (
-			<>
+			<div style={{ height: '100vh' }}>
 				<Header />
-				<h3>Top Viewed Products</h3>
-				<div
-					style={{
-						height: '100vh',
-						display: 'flex',
-						flexDirection: 'column',
-						marginLeft: '30vw',
-						marginTop: '10vh',
-					}}
-				>
-					{this.state.sortedList.map((item) => (
-						<div
-							style={{
-								backgroundColor: '#feffff',
-								boxShadow:
-									'0 5px 10px rgba(154, 160, 185, 0.05),0 15px 40px rgba(166, 173, 201, 0.2)',
-								border: 'none',
-								marginBottom: '10px',
-								borderRadius: '5px',
-								display: 'flex',
-								maxWidth: '40vw',
-								padding: '5px 15px',
-								justifyContent: 'space-between',
-								textAlign: 'left',
-							}}
-						>
-							<p>Product Name : </p>{' '}
-							<p style={{ fontWeight: 'bold', textAlign: 'left' }}>
-								{item.name}
-							</p>
-							<p>Count of Views</p>{' '}
-							<p style={{ fontWeight: 'bold' }}>{item.count}</p>
-						</div>
-					))}
-				</div>
-			</>
+				<Container className='mt-5'>
+					<Row>
+						<Col md={{ span: 6, offset: 3 }}>
+							<Pie
+								data={this.state.data}
+								options={{
+									title: {
+										display: true,
+										text: 'Top Viewed Products',
+										fontSize: 20,
+									},
+									legend: {
+										display: true,
+										position: 'right',
+									},
+								}}
+							/>
+						</Col>
+					</Row>
+				</Container>
+			</div>
 		);
 	}
 }
-
-function compare(a, b) {
-	if (a.count > b.count) {
-		return -1;
+function getBackgroundColors(lengthOfArr) {
+	let colors = [];
+	for (let i = 0; i < lengthOfArr; i++) {
+		colors.push('#' + Math.floor(Math.random() * 16777215).toString(16));
 	}
-	if (a.count < b.count) {
-		return 1;
-	}
-	return 0;
+	return colors;
 }
